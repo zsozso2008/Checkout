bf257bc8ac9e0b755a918ee084f9a35a244ff6a9
zsozso2008 <zsozso2008@yahoo.com>
2022-08-08 18:24:51 +0300

    Update BasketDbContextFactory.cs

--- Checkout.DataMigrations/BasketDbContextFactory.cs
+++ Checkout.DataMigrations/BasketDbContextFactory.cs
@@ -11,7 +11,7 @@ namespace Checkout.DataMigrations
         public BasketDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<BasketDbContext>();
-            var connStr = "Server=tcp:routingimport.database.windows.net,1433;Initial Catalog=routing_importer;Persist Security Info=False;User ID=hermestest;Password=routingtest2021!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            var connStr = "Data Source=.;Initial Catalog=Checkout;Integrated Security=True;";
             optionsBuilder.UseSqlServer(connStr, b => b.MigrationsAssembly("Checkout.DataMigrations"));
 
             return new BasketDbContext(optionsBuilder.Options);
